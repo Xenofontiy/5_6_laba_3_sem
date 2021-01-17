de3caf5b4caf9f34373267c1a16ccca8a258d1a7
Xenofontiy <xenofontiy@gmail.com>
2021-01-17 21:12:25 +0300

    поправлены некоторые вылеты

--- 5_6_laba_sem/MainWindow.xaml.cs
+++ 5_6_laba_sem/MainWindow.xaml.cs
@@ -12,7 +12,7 @@ namespace _5_6_laba_sem
     /// </summary>
     public partial class MainWindow : Window
     {
-        string Path_inf; int Max_Prise, quantity;
+        string Path_inf; int Max_Prise, quantity, numer_;
         _Inf_struct _Inf_data = new _Inf_struct();
         List<Inf_struct> Inf_data = new List<Inf_struct>();
 
@@ -35,58 +35,63 @@ namespace _5_6_laba_sem
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-
-            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();              //Открытие окошка где происходит поиск
-            dlg.DefaultExt = ".txt";
-            Nullable<bool> result = dlg.ShowDialog();
-            if (result == true)
+            try
             {
-                Path_inf = dlg.FileName;
                 Food_List_ListView.Items.Clear();
-            }
-
-            int numer = 0;
-            using (StreamReader sr = new StreamReader(Path_inf, System.Text.Encoding.Default))      //чтение и разбивка по классам
-            {
-
-                string line, _line;
-                while ((line = sr.ReadLine()) != null)
+                Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();              //Открытие окошка где происходит поиск
+                dlg.DefaultExt = ".txt";
+                dlg.Filter = "txt files (*.txt)|*.txt";
+                Nullable<bool> result = dlg.ShowDialog();
+                if (result == true)
                 {
-                    _line = line;
-                    while ((_line.IndexOf(":") > -1))
+                    Path_inf = dlg.FileName;
+                    Food_List_ListView.Items.Clear();
+                }
+
+                int numer = 0;
+                using (StreamReader sr = new StreamReader(Path_inf, System.Text.Encoding.Default))      //чтение и разбивка по классам
+                {
+
+                    string line, _line;
+                    while ((line = sr.ReadLine()) != null)
                     {
-                        if (numer == 0)
+                        _line = line;
+                        while ((_line.IndexOf(":") > -1))
                         {
-                            _Inf_data._Inf_category = _line.Substring(0, _line.IndexOf(":"));
-                            ++numer;
-                        }
-                        else if (numer == 1)
-                        {
-                            _Inf_data._Inf_name = _line.Substring(0, _line.IndexOf(":"));
-                            ++numer;
-                        }
-                        else if (numer == 2)
-                        {
-                            _Inf_data._Inf_usefulness = Convert.ToInt32(_line.Substring(0, _line.IndexOf(":")));
-                            ++numer;
-                            _line = _line.Remove(0, (_line.IndexOf(":")) + 1);
+                            if (numer == 0)
+                            {
+                                _Inf_data._Inf_category = _line.Substring(0, _line.IndexOf(":"));
+                                ++numer;
+                            }
+                            else if (numer == 1)
+                            {
+                                _Inf_data._Inf_name = _line.Substring(0, _line.IndexOf(":"));
+                                ++numer;
+                            }
+                            else if (numer == 2)
+                            {
+                                _Inf_data._Inf_usefulness = Convert.ToInt32(_line.Substring(0, _line.IndexOf(":")));
+                                ++numer;
+                                _line = _line.Remove(0, (_line.IndexOf(":")) + 1);
 
 
-                            _Inf_data._Inf_price = Convert.ToInt32(_line);
-                            numer = 0;
+                                _Inf_data._Inf_price = Convert.ToInt32(_line);
+                                numer = 0;
 
-                            _line = " ";
+                                _line = " ";
+                            }
+                            if (numer != 0)
+                                _line = _line.Remove(0, (_line.IndexOf(":")) + 1);
                         }
-                        if (numer != 0)
-                            _line = _line.Remove(0, (_line.IndexOf(":")) + 1);
-                    }
-                    var Inf_data = new List<Inf_struct>()
+                        var Inf_data = new List<Inf_struct>()
                     {
                        new Inf_struct(_Inf_data._Inf_category, _Inf_data._Inf_name, _Inf_data._Inf_price, _Inf_data._Inf_usefulness)
                     };
-                    Food_List_ListView.Items.Add(_Inf_data._Inf_category + ':' + _Inf_data._Inf_name + ':' + _Inf_data._Inf_price + ':' + _Inf_data._Inf_usefulness);
+                        Food_List_ListView.Items.Add(_Inf_data._Inf_category + ':' + _Inf_data._Inf_name + ':' + _Inf_data._Inf_price + ':' + _Inf_data._Inf_usefulness);
+                    }
                 }
             }
+            catch { };
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
@@ -96,30 +101,34 @@ namespace _5_6_laba_sem
             {
                 quantity = Convert.ToInt32(quantity_TextBox.Text);
                 Max_Prise = Convert.ToInt32(Max_prise_TextBox.Text);
+
+
+                ItemCollection[] ic = new ItemCollection[quantity + 1];
+                for (int i = 0; i <= quantity; i++)
+                    ic[i] = new ItemCollection();
+                for (int i = 0; i < Inf_data.Count; i++)
+                    for (int j = quantity; j >= 0; j--)
+                        if (j >= Inf_data[i].price)
+                        {
+                            int _Quantity = Math.Min(quantity, j / Inf_data[i].price);
+                            for (int k = 1; k <= _Quantity; k++)
+                            {
+                                ItemCollection lighterCollection = ic[j - k * Inf_data[i].price];
+                                int testValue = lighterCollection.TotalUsefulness + k * Inf_data[i].usefulness;
+                                if (testValue > ic[j].TotalUsefulness)
+                                {
+                                    (ic[j] = lighterCollection.Copy()).AddItem(Inf_data[i], k);
+                                    numer_ = j;
+                                }
+                            }
+                        }
+                Basket_ListView.ItemsSource = ic;
             }
             catch
             {
-                MessageBox.Show("Введите корректно числа");
+                if (Food_List_ListView.ItemsSource == null) MessageBox.Show("Укажите текстовый файл");
+                else MessageBox.Show("Введите корректно числа");
             }
-
-            ItemCollection[] ic = new ItemCollection[quantity + 1];
-            for (int i = 0; i <= quantity; i++)
-                ic[i] = new ItemCollection();
-            for (int i = 0; i < Inf_data.Count; i++)
-                for (int j = quantity; j >= 0; j--)
-                    if (j >= Inf_data[i].price)
-                    {
-                        int _Quantity = Math.Min(quantity, j / Inf_data[i].price);
-                        for (int k = 1; k <= _Quantity; k++)
-                        {
-                            ItemCollection lighterCollection = ic[j - k * Inf_data[i].price];
-                            int testValue = lighterCollection.TotalUsefulness + k * Inf_data[i].usefulness;
-                            if (testValue > ic[j].TotalUsefulness) {
-                                (ic[j] = lighterCollection.Copy()).AddItem(Inf_data[i], k);
-                            }
-                        }
-                    }
-            Basket_ListView.ItemsSource = ic;
 
         }
     }
